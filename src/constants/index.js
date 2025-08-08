efa1697f5203112e1c5ffd1c54ed1c58af3bea0d
Galushko Evgeniy <yudginchik@gmail.com>
2025-08-08 13:18:16 +0300

    swagger

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,4 +2,4 @@ import path from 'node:path';
 
 export const THIRTY_DAYS = 30 * 24 * 60 * 60 * 1000;
 
-export const SWAGGER_PATH = path.json(process.cmd(), 'docs', 'swagger.json');
+export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
