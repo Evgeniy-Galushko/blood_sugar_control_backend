c1b4a26a3507684fdcc6193e1f86ccc81d1a1132
Galushko Evgeniy <yudginchik@gmail.com>
2025-08-14 17:06:51 +0300

    fix

--- src/validation/measurement.js
+++ src/validation/measurement.js
@@ -1,5 +1,29 @@
 import Joi from 'joi';
 
+// export const validationAddMeasurementSchema = Joi.object({
+//   date: Joi.string().min(8).max(12).required().messages({
+//     'string.base': 'Day must be a string!',
+//     'string.min': 'The minimum number of characters per day is 8!',
+//     'string.max': 'The maximum number of characters in a day is 12!',
+//   }),
+//   time: Joi.string().min(2).max(8).required().messages({
+//     'string.base': 'Time must be a string!',
+//     'string.min': 'The minimum number of characters per time is 2!',
+//     'string.max': 'The maximum number of characters in a eime is 8!',
+//   }),
+//   sugarLevel: Joi.number().required().messages({
+//     'string.base': 'Sugar level must be a number!',
+//   }),
+//   measurementTime: Joi.string()
+//     .valid('натощак', 'после еды')
+//     .default('натощак')
+//     .required()
+//     .messages({
+//       'string.base': 'Measurement time must be a string!',
+//     }),
+//   userId: Joi.string(),
+// });
+
 export const validationAddMeasurementSchema = Joi.object({
   date: Joi.string().min(8).max(12).required().messages({
     'string.base': 'Day must be a string!',
@@ -11,7 +35,10 @@ export const validationAddMeasurementSchema = Joi.object({
     'string.min': 'The minimum number of characters per time is 2!',
     'string.max': 'The maximum number of characters in a eime is 8!',
   }),
-  sugarLevel: Joi.number().required().messages({
+  onAnEmptyStomach: Joi.number().messages({
+    'string.base': 'Sugar level must be a number!',
+  }),
+  afterEating: Joi.number().messages({
     'string.base': 'Sugar level must be a number!',
   }),
   measurementTime: Joi.string()
